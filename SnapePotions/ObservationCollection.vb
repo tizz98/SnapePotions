b3fc9602ae72cef3690f9192652c77ecd82463c8
tizz98 <dev.tizz98@gmail.com>
2016-01-30 08:34:56 -0800

    observation collection docs

--- SnapePotions/ObservationCollection.vb
+++ SnapePotions/ObservationCollection.vb
@@ -1,4 +1,16 @@
-﻿Public Class ObservationCollection
+﻿'------------------------------------------------------------
+'-             File Name: ObservationCollection             -
+'-                 Part of Project: Assign3                 -
+'------------------------------------------------------------
+'-                Written By: Elijah Wilson                 -
+'-                  Written On: 01/30/2016                  -
+'------------------------------------------------------------
+'- File Purpose:                                            -
+'-                                                          -
+'- Contains the ObservationCollection class, which has      -
+'- aggregate data of the Observations added to it.          -
+'------------------------------------------------------------
+Public Class ObservationCollection
     Public totalCount As Integer = 0
     Public underCount As Integer = 0
     Public overCount As Integer = 0
@@ -12,6 +24,25 @@
     Private minObservationSet As Boolean = False
     Private maxObservationSet As Boolean = False
 
+    '------------------------------------------------------------
+    '-                   Subprogram Name: Add                   -
+    '------------------------------------------------------------
+    '-                Written By: Elijah Wilson                 -
+    '-                  Written On: 01/30/2016                  -
+    '------------------------------------------------------------
+    '- Subprogram Purpose:                                      -
+    '-                                                          -
+    '- Add a new Observation to this collection. It also        -
+    '- updates certain statistics based on the Observation      -
+    '- itself.                                                  -
+    '------------------------------------------------------------
+    '- Parameter Dictionary (in parameter order):               -
+    '- observation - The Observation being added to the         -
+    '-               collection                                 -
+    '------------------------------------------------------------
+    '- Local Variable Dictionary (alphabetically):              -
+    '- (None)                                                   -
+    '------------------------------------------------------------
     Public Sub Add(observation As Observation)
         observations.Add(observation)
 
@@ -41,6 +72,28 @@
         End If
     End Sub
 
+    '------------------------------------------------------------
+    '-                Function Name: getAverage                 -
+    '------------------------------------------------------------
+    '-                Written By: Elijah Wilson                 -
+    '-                  Written On: 01/30/2016                  -
+    '------------------------------------------------------------
+    '- Function Purpose:                                        -
+    '-                                                          -
+    '- This gets the average observation values based on the    -
+    '- accumulated total and the total count. If there aren't   -
+    '- any observations added yet, it returns 0.0               -
+    '------------------------------------------------------------
+    '- Parameter Dictionary (in parameter order):               -
+    '- (None)                                                   -
+    '------------------------------------------------------------
+    '- Local Variable Dictionary (alphabetically):              -
+    '- (None)                                                   -
+    '------------------------------------------------------------
+    '- Returns:                                                 -
+    '- Decimal - The average observation value or 0.0 if there  -
+    '-           aren't any observations added yet              -
+    '------------------------------------------------------------
     Public Function getAverage() As Decimal
         Return IIf(totalCount = 0, 0.0, observationAccumulatedTotal / totalCount)
     End Function
