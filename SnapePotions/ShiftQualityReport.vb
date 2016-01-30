239cdb55ad30298b5a6f707cd48ae9db51bf8f0e
tizz98 <dev.tizz98@gmail.com>
2016-01-30 08:50:48 -0800

    ShiftQualityReport docs

--- SnapePotions/ShiftQualityReport.vb
+++ SnapePotions/ShiftQualityReport.vb
@@ -1,4 +1,16 @@
-﻿Imports System.IO
+﻿'------------------------------------------------------------
+'-             File Name: ShiftQualityReport.vb             -
+'-                 Part of Project: Assign3                 -
+'------------------------------------------------------------
+'-                Written By: Elijah Wilson                 -
+'-                  Written On: 01/30/2016                  -
+'------------------------------------------------------------
+'- File Purpose:                                            -
+'-                                                          -
+'- Contains the ShiftQualityReport class, which manages     -
+'- generating the report as well as saving it to a file.    -
+'------------------------------------------------------------
+Imports System.IO
 
 Public Class ShiftQualityReport
     Private histogram As Histogram
@@ -17,6 +29,29 @@ Public Class ShiftQualityReport
     Private OBSERVATION_LINE_FORMAT As String
     Private HEADER_FOOTER_DIVIDER_LINE As String = StrDup(FULL_LINE_LENGTH, HEADER_FOOTER_DIVIDER_CHAR)
 
+    '------------------------------------------------------------
+    '-                   Subprogram Name: New                   -
+    '------------------------------------------------------------
+    '-                Written By: Elijah Wilson                 -
+    '-                  Written On: 01/30/2016                  -
+    '------------------------------------------------------------
+    '- Subprogram Purpose:                                      -
+    '-                                                          -
+    '- Creates a new ShiftQualityReport object. It also creates -
+    '- a new Histogram object to be used when generating the    -
+    '- report.                                                  -
+    '------------------------------------------------------------
+    '- Parameter Dictionary (in parameter order):               -
+    '- shiftManager - The ShiftManager object to be used for    -
+    '-                generating the report                     -
+    '- headerCorpTitle - The name of the corporation to be used -
+    '-                   in the heading                         -
+    '- histogramLineLength - Optional: The max line length to   -
+    '-                       be used when generating lines      -
+    '------------------------------------------------------------
+    '- Local Variable Dictionary (alphabetically):              -
+    '- (None)                                                   -
+    '------------------------------------------------------------
     Public Sub New(shiftManager As ShiftManager, headerCorpTitle As String,
                    Optional histogramLineLength As Integer = Histogram.DEFAULT_LINE_LENGTH)
         Me.shiftManager = shiftManager
@@ -25,6 +60,26 @@ Public Class ShiftQualityReport
         Me.OBSERVATION_LINE_FORMAT = "({0,-4}) {1,4} {2, -" & histogramLineLength & "}"
     End Sub
 
+    '------------------------------------------------------------
+    '-              Function Name: generateReport               -
+    '------------------------------------------------------------
+    '-                Written By: Elijah Wilson                 -
+    '-                  Written On: 01/30/2016                  -
+    '------------------------------------------------------------
+    '- Function Purpose:                                        -
+    '-                                                          -
+    '- Generate a report based on the shiftManager data.        -
+    '------------------------------------------------------------
+    '- Parameter Dictionary (in parameter order):               -
+    '- (None)                                                   -
+    '------------------------------------------------------------
+    '- Local Variable Dictionary (alphabetically):              -
+    '- returnString - The string to be returned, accumulated    -
+    '-                over time with additional report data     -
+    '------------------------------------------------------------
+    '- Returns:                                                 -
+    '- String - A string containing the report                  -
+    '------------------------------------------------------------
     Public Function generateReport() As String
         Dim returnString As String = getReportHeading() & getObservationHistogram()
         returnString &= getShiftInfoHeading() & getShiftInformation()
@@ -32,6 +87,27 @@ Public Class ShiftQualityReport
         Return returnString
     End Function
 
+    '------------------------------------------------------------
+    '-             Function Name: getReportHeading              -
+    '------------------------------------------------------------
+    '-                Written By: Elijah Wilson                 -
+    '-                  Written On: 01/30/2016                  -
+    '------------------------------------------------------------
+    '- Function Purpose:                                        -
+    '-                                                          -
+    '- Generates a heading for the report.                      -
+    '------------------------------------------------------------
+    '- Parameter Dictionary (in parameter order):               -
+    '- (None)                                                   -
+    '------------------------------------------------------------
+    '- Local Variable Dictionary (alphabetically):              -
+    '- returnString - The string to be returned, accumulated    -
+    '-                over time with more data for this         -
+    '-                function                                  -
+    '------------------------------------------------------------
+    '- Returns:                                                 -
+    '- String - A string containing the report heading          -
+    '------------------------------------------------------------
     Private Function getReportHeading() As String
         Dim returnString As String = HEADER_FOOTER_DIVIDER_LINE & vbCrLf
 
@@ -42,6 +118,28 @@ Public Class ShiftQualityReport
         Return returnString
     End Function
 
+    '------------------------------------------------------------
+    '-            Function Name: getShiftInfoHeading            -
+    '------------------------------------------------------------
+    '-                Written By: Elijah Wilson                 -
+    '-                  Written On: 01/30/2016                  -
+    '------------------------------------------------------------
+    '- Function Purpose:                                        -
+    '-                                                          -
+    '- Generates a shift information heading for the report.    -
+    '------------------------------------------------------------
+    '- Parameter Dictionary (in parameter order):               -
+    '- (None)                                                   -
+    '------------------------------------------------------------
+    '- Local Variable Dictionary (alphabetically):              -
+    '- returnString - The string to be returned, accumulated    -
+    '-                over time with more data for this         -
+    '-                function                                  -
+    '------------------------------------------------------------
+    '- Returns:                                                 -
+    '- String - A string containing the shift information       -
+    '-          heading                                         -
+    '------------------------------------------------------------
     Private Function getShiftInfoHeading() As String
         Dim returnString As String = HEADER_FOOTER_DIVIDER_LINE & vbCrLf
         returnString &= centerString(SHIFT_INFO_HEADER_TITLE) & vbCrLf
@@ -50,6 +148,29 @@ Public Class ShiftQualityReport
         Return returnString
     End Function
 
+    '------------------------------------------------------------
+    '-            Function Name: getShiftInformation            -
+    '------------------------------------------------------------
+    '-                Written By: Elijah Wilson                 -
+    '-                  Written On: 01/30/2016                  -
+    '------------------------------------------------------------
+    '- Function Purpose:                                        -
+    '-                                                          -
+    '- Generates shift information for the report. Including:   -
+    '- the observation information for each shift and the       -
+    '- statistics for each shift.                               -
+    '------------------------------------------------------------
+    '- Parameter Dictionary (in parameter order):               -
+    '- (None)                                                   -
+    '------------------------------------------------------------
+    '- Local Variable Dictionary (alphabetically):              -
+    '- returnString - The string to be returned, accumulated    -
+    '-                over time with more data for this         -
+    '-                function                                  -
+    '------------------------------------------------------------
+    '- Returns:                                                 -
+    '- String - A string containing the shift information       -
+    '------------------------------------------------------------
     Private Function getShiftInformation() As String
         Dim returnString As String = getObservationShiftInformation() & vbCrLf
         returnString &= getShiftStatisticsInformation() & vbCrLf
@@ -58,6 +179,36 @@ Public Class ShiftQualityReport
         Return returnString
     End Function
 
+    '------------------------------------------------------------
+    '-      Function Name: getObservationShiftInformation       -
+    '------------------------------------------------------------
+    '-                Written By: Elijah Wilson                 -
+    '-                  Written On: 01/30/2016                  -
+    '------------------------------------------------------------
+    '- Function Purpose:                                        -
+    '-                                                          -
+    '- Generates observation information for each shift. This   -
+    '- includes: number of observations, number under target,   -
+    '- number on target and number over target.                 -
+    '------------------------------------------------------------
+    '- Parameter Dictionary (in parameter order):               -
+    '- (None)                                                   -
+    '------------------------------------------------------------
+    '- Local Variable Dictionary (alphabetically):              -
+    '- firstStr - Contains the word to be used to describe the  -
+    '-            first shift                                   -
+    '- returnString - The string to be returned, accumulated    -
+    '-                over time with more data for this         -
+    '-                function                                  -
+    '- secondStr - Contains the word to be used to describe the -
+    '-             second shift                                 -
+    '- thirdStr - Contains the word to be used to describe the  -
+    '-            third shift                                   -
+    '------------------------------------------------------------
+    '- Returns:                                                 -
+    '- String - A string containing the shift information for   -
+    '-          each shift                                      -
+    '------------------------------------------------------------
     Private Function getObservationShiftInformation() As String
         Dim firstStr As String = "First"
         Dim secondStr As String = "Second"
@@ -86,6 +237,30 @@ Public Class ShiftQualityReport
         Return returnString
     End Function
 
+    '------------------------------------------------------------
+    '-       Function Name: getShiftStatisticsInformation       -
+    '------------------------------------------------------------
+    '-                Written By: Elijah Wilson                 -
+    '-                  Written On: 01/30/2016                  -
+    '------------------------------------------------------------
+    '- Function Purpose:                                        -
+    '-                                                          -
+    '- Generates observation statistics for each shift. This    -
+    '- includes: minimum observed, average observed and maximum -
+    '- observed.                                                -
+    '------------------------------------------------------------
+    '- Parameter Dictionary (in parameter order):               -
+    '- (None)                                                   -
+    '------------------------------------------------------------
+    '- Local Variable Dictionary (alphabetically):              -
+    '- returnString - The string to be returned, accumulated    -
+    '-                over time with more data for this         -
+    '-                function                                  -
+    '------------------------------------------------------------
+    '- Returns:                                                 -
+    '- String - A string containing the shift statistics for    -
+    '-          each shift                                      -
+    '------------------------------------------------------------
     Private Function getShiftStatisticsInformation() As String
         Dim returnString As String = String.Format(SHIFT_STATS_FMT_STR, "Statistics:", "", "", "") & vbCrLf
         returnString &= String.Format(SHIFT_STATS_FMT_STR, "Minimum Observed:",
@@ -104,6 +279,29 @@ Public Class ShiftQualityReport
         Return returnString
     End Function
 
+    '------------------------------------------------------------
+    '-          Function Name: getObservationHistogram          -
+    '------------------------------------------------------------
+    '-                Written By: Elijah Wilson                 -
+    '-                  Written On: 01/30/2016                  -
+    '------------------------------------------------------------
+    '- Function Purpose:                                        -
+    '-                                                          -
+    '- Generates a histogram based on the observations. These   -
+    '- are sorted in ascending order.                           -
+    '------------------------------------------------------------
+    '- Parameter Dictionary (in parameter order):               -
+    '- (None)                                                   -
+    '------------------------------------------------------------
+    '- Local Variable Dictionary (alphabetically):              -
+    '- returnString - The string to be returned, accumulated    -
+    '-                over time with more data for this         -
+    '-                function                                  -
+    '------------------------------------------------------------
+    '- Returns:                                                 -
+    '- String - A string containing the histogram for all of    -
+    '-          the observations                                -
+    '------------------------------------------------------------
     Private Function getObservationHistogram() As String
         Dim returnString As String = OBSERVATION_HEADER_LINE & vbCrLf
 
@@ -116,11 +314,50 @@ Public Class ShiftQualityReport
         Return returnString
     End Function
 
+    '------------------------------------------------------------
+    '-               Function Name: centerString                -
+    '------------------------------------------------------------
+    '-                Written By: Elijah Wilson                 -
+    '-                  Written On: 01/30/2016                  -
+    '------------------------------------------------------------
+    '- Function Purpose:                                        -
+    '-                                                          -
+    '- Centers a specified string in white space.               -
+    '------------------------------------------------------------
+    '- Parameter Dictionary (in parameter order):               -
+    '- stringToCenter - The string that should be centered      -
+    '- lineLength - Optional: The length of the line that is    -
+    '-              being centered.                             -
+    '------------------------------------------------------------
+    '- Local Variable Dictionary (alphabetically):              -
+    '- (None)                                                   -
+    '------------------------------------------------------------
+    '- Returns:                                                 -
+    '- String - A string containing the stringToCenter,         -
+    '-          centered in spaces.                             -
+    '------------------------------------------------------------
     Public Function centerString(stringToCenter As String, Optional lineLength As Integer = FULL_LINE_LENGTH) As String
         Return String.Format("{0,-" & CStr(lineLength) & "}",
                              String.Format("{0," & (Math.Ceiling((lineLength + stringToCenter.Length) / 2)).ToString() & "}", stringToCenter))
     End Function
 
+    '------------------------------------------------------------
+    '-            Subprogram Name: writeReportToFile            -
+    '------------------------------------------------------------
+    '-                Written By: Elijah Wilson                 -
+    '-                  Written On: 01/30/2016                  -
+    '------------------------------------------------------------
+    '- Subprogram Purpose:                                      -
+    '-                                                          -
+    '- This generates the report and writes it to the specified -
+    '- file path.                                               -
+    '------------------------------------------------------------
+    '- Parameter Dictionary (in parameter order):               -
+    '- filePath - Where to write the report to                  -
+    '------------------------------------------------------------
+    '- Local Variable Dictionary (alphabetically):              -
+    '- file - The StreamWriter object of the input filePath     -
+    '------------------------------------------------------------
     Public Sub writeReportToFile(filePath As String)
         Dim file As StreamWriter = My.Computer.FileSystem.OpenTextFileWriter(filePath, False)  ' Overwrite if the file exists
         file.Write(generateReport())
